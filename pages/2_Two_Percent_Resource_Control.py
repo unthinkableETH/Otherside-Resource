6bc7646e653264a363395e1a24dff6f342ee0fbd
Shaun <158432022+unthinkableETH@users.noreply.github.com>
2024-04-15 13:56:18 -0700

    Update 2_Two_Percent_Resource_Control.py

--- pages/2_Two_Percent_Resource_Control.py
+++ pages/2_Two_Percent_Resource_Control.py
@@ -65,10 +65,10 @@ def make_graph(data_df,percent_v):
     p.toolbar.active_scroll = None
     p.toolbar.active_tap = None   
     p.yaxis.formatter = BasicTickFormatter(use_scientific=False)
-    p.yaxis.ticker.base = 10
+    p.yaxis.ticker.base = 5
     p.yaxis.ticker.desired_num_ticks=10
     p.yaxis.ticker.max_interval=10
-    p.yaxis.ticker.mantissas=[1,2,5]
+    p.yaxis.ticker.mantissas=[1,2,5,10]
     return(placeholder.bokeh_chart(p, use_container_width=True))
 
 
